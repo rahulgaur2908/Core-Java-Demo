d6644508819089185aa6e9aa6ecf2fd4faeb2eec
Rahul Gaur <rahulgaur0102@gmail.com>
2026-04-10 21:16:40 +0530

    Add files via upload

---
+++ Arraysorting.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Arraysorting {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 8, 1, 9};
+   
+   System.out.println("Array contents: ");
+    Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i] + "");
+        }
+    }
+}  
+       
+   
+
+       
+ 
+
+
+
+
